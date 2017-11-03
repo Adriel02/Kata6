14b239fd53f39edc6076b3e03c6ff191cf34abdb
Adriel Garcia Diaz <32568729+Adriel02@users.noreply.github.com>
2017-11-03 12:43:48 +0100

    VERSION 2 KATA 4

--- main/Kata4.java
+++ main/Kata4.java
@@ -10,11 +10,30 @@ import java.io.IOException;
 import java.util.List;
 
 public class Kata4 {
+    public List<Mail> mailList;
+    Histogram<String> histogram;
+
     public static void main(String[] args) throws IOException {
+        Kata4 kata4=new Kata4();
+        kata4.execute();
+    }
+
+    private void input() throws IOException {
         String filename ="C:\\Users\\ADRIELGARCÍADÍAZ\\IdeaProjects\\Kata4\\emails.txt";
-        List<Mail> mailList= MailListReader.read(filename);
-        Histogram<String> histogram = MailHistogramBuilder.build(mailList);
+        mailList= MailListReader.read(filename);
+    }
+
+    private void process(){
+        histogram = MailHistogramBuilder.build(mailList);
+    }
+
+    private void output(){
         HistogramDisplay histoDisplay= new HistogramDisplay(histogram);
-        histoDisplay.execute();
+    }
+
+    private void execute() throws IOException {
+        input();
+        process();
+        output();
     }
 }
